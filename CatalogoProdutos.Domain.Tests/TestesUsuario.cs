4eb3236c6af37dea0f9904fe189e7c8980d7c822
joaobrrj91 <joao91.nascimento@gmail.com>
2018-08-10 19:58:39 -0300

    Criação dos primeiros testes no Domain.Tests Alteração na classe Usuario para conter novos metodos de validação

--- CatalogoProdutos.Domain.Tests/TestesUsuario.cs
+++ CatalogoProdutos.Domain.Tests/TestesUsuario.cs
@@ -9,36 +9,60 @@ namespace CatalogoProdutos.Domain.Tests
         [TestMethod]
         [TestCategory("Novo usuário")]
         [ExpectedException(typeof(Exception), noExceptionMessage: "O nome deve ser valido.Tratar erro")]
-        public void O_Usuario_deve_conter_o_nome()
+        public void O_Usuario_deve_conter_o_nome_valido()
         {
-            var usuario = new Usuario(nome: "",email:"joao91.nascimento@gmail.com",usuarioAcesso:"joaozinho",senha: "1234");
+            var usuario = new Usuario(nome: "",email:"joao91.nascimento@gmail.com",usuarioAcesso:"joaozinho",senha: "12345679");
         }
 
 
         [TestMethod]
         [TestCategory("Novo usuário")]
         [ExpectedException(typeof(Exception),noExceptionMessage: "O email deve ser informado.Tratar erro")]
-        public void O_Usuario_deve_conter_o_email()
+        public void O_Usuario_deve_conter_o_email_valido()
         {
-            var usuario = new Usuario(nome: "joao", email: "joao91.nascimento@2@sasgmail.com.br.br", usuarioAcesso: "joaozinho", senha: "1234");
+            var usuario = new Usuario(nome: "joao", email: "joao91.nascimento@@gmail.com.br", usuarioAcesso: "joaozinho", senha: "123464654");
         }
 
 
         [TestMethod]
         [TestCategory("Novo usuário")]
         [ExpectedException(typeof(Exception), noExceptionMessage: "O usuario de acesso deve ser informado.Tratar erro")]
-        public void O_Usuario_deve_conter_o_usuario_de_acesso()
+        public void O_Usuario_deve_conter_o_usuario_de_acesso_valido()
         {
-            var usuario = new Usuario(nome: "joao", email: "joao91.nascimento@gmail.com", usuarioAcesso: "", senha: "1234");
+            var usuario = new Usuario(nome: "joao", email: "joao91.nascimento@gmail.com", usuarioAcesso: "", senha: "1245646534");
         }
 
 
         [TestMethod]
         [TestCategory("Novo usuário")]
         [ExpectedException(typeof(Exception), noExceptionMessage: "A senha deve ser informado.Tratar erro")]
-        public void O_Usuario_deve_conter_a_senha()
+        public void O_Usuario_deve_conter_a_senha_valido()
         {
             var usuario = new Usuario(nome: "joao", email: "joao91.nascimento@gmail.com", usuarioAcesso: "joaozinho", senha: "");
         }
     }
+
+    [TestClass]
+    public class Ao_alterar_os_dados_do_usuario
+    {
+        [TestMethod]
+        [TestCategory("Alterar E-mail")]
+        [ExpectedException(typeof(Exception), noExceptionMessage: "O novo email deve ser validado.Tratar erro")]
+        public void O_novo_email_deve_ser_valido()
+        {
+            var usuario = new Usuario(nome: "joao nascimento", email: "joao91.nascimento@gmail.com", usuarioAcesso: "joaozinho", senha: "12345789");
+            usuario.AlterarEmail("joao91.nascimentogmail.com.br");
+        }
+
+        [TestMethod]
+        [TestCategory("Alterar Senha")]
+        [ExpectedException(typeof(Exception), noExceptionMessage: "A nova senha deve ser validada.Tratar erro")]
+        public void O_usuario_e_a_nova_senham_deve_ser_validos()
+        {
+            var usuario = new Usuario(nome: "joao nascimento", email: "joao91.nascimento@gmail.com", usuarioAcesso: "joaozinho", senha: "12345789");
+            usuario.AlterarSenha("joaozinho2", "12345789", "123457894", "123457894");
+        }
+
+    }
+
 }
--- CatalogoProdutos.Domain/Usuario.cs
+++ CatalogoProdutos.Domain/Usuario.cs
@@ -1,42 +1,104 @@
-﻿using System;
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
 
 namespace CatalogoProdutos.Domain
 {
+    //TODO: Criar uma classe para validar esses testes semelhante ao Contracts - Refatoração para uma classe de validação
     public class Usuario
     {
-
+        #region Construtores
         protected Usuario()
         {
 
         }
 
-        public Usuario(
-            string nome,
-            string email,
-            string usuarioAcesso,
-            string senha)
+        public Usuario(string nome, string email, string usuarioAcesso, string senha)
         {
 
             //Contract.Requires<Exception>(nome.Length > 3, "Nome Invalido");
-
-            //TODO: Criar uma classe para validar esses testes
+   
             if (nome.Length < 3)
-                throw new System.Exception("O nome deve ser maior que 3 caracteres");
+                throw new System.Exception("O nome deve ser no minimo 3 caracteres");
 
-            if (!Regex.IsMatch(email, @"^[A-Za-z0-9](([_\.\-]?[a-zA-Z0-9]+)*)@([A-Za-z0-9]+)(([\.\-]?[a-zA-Z0-9]+)*)\.([A-Za-z]{2,})$"))
+            if (!Regex.IsMatch(email, @"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$"))
                 throw new System.Exception("O email deve ser valido");
 
+            if (usuarioAcesso.Length < 8)
+                throw new System.Exception("O usuário de acesso deve ser no minimo 8 caracteres");
 
+            if (senha.Length < 6)
+                throw new System.Exception("A senha deve ser no minimo 6 caracteres");
 
+            Id = 0;
+            Nome = nome;
+            Email = email;
+            UsuarioAcesso = usuarioAcesso;
+            Senha = senha;
+        }
+        #endregion
+
+        #region Propriedades
+        public int Id  { get; protected set; }
+        public string Nome  { get; protected set; }
+        public string Email { get; protected set; }
+        public string UsuarioAcesso { get; protected set; }
+        public string Senha { get; protected set; }
+        #endregion
+
+        #region Métodos Públicos
+
+        public void AlterarEmail(string email)
+        {
+           if (!Regex.IsMatch(email, @"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$"))
+                throw new System.Exception("O email deve ser valido");
+            Email = email;
         }
 
-        public int Id { get; set; }
-        public string Nome { get; set; }
-        public string Email { get; set; }
-        public string Imagem { get; set; }
-        public string UsuarioAcesso { get; set; }
-        public string Senha { get; set; }
+
+        public void AlterarSenha(string usuarioAcesso, string senha, string novaSenha,string confirmacaoSenha)
+        {
+            //TODO: Fazer um cenario de teste para cada if : METODO AlterarSenha
+            if (!UsuarioAcesso.Equals(usuarioAcesso, System.StringComparison.InvariantCultureIgnoreCase) || !Senha.Equals(senha))
+                throw new System.Exception("Usuário ou senha incorretos");
+
+            if(Senha.Equals(novaSenha))
+                throw new System.Exception("Nova senha deve ser diferente da atual");
+
+            if (senha.Length < 6)
+                throw new System.Exception("A senha deve ser no minimo 6 caracteres");
+
+            if(!novaSenha.Equals(confirmacaoSenha))
+                throw new System.Exception("As senhas digitadas não coincidem");
+
+            Senha = novaSenha;
+        }
+
+        public void Autenticar(string usuarioAcesso, string senha)
+        {
+            if (!UsuarioAcesso.Equals(usuarioAcesso, System.StringComparison.InvariantCultureIgnoreCase) || !Senha.Equals(senha))
+                throw new System.InvalidOperationException("Usuário ou senha incorretos");
+        }
+
+        public void Registrar(string nome, string email, string usuarioAcesso, string senha, string confirmacaoSenha)
+        {
+
+            //TODO: Fazer um cenario de teste para cada if: METODO Registrar
+            if (nome.Length < 3)
+                throw new System.InvalidCastException("O nome deve ser no minimo 3 caracteres");
+
+            if (!Regex.IsMatch(email, @"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$"))
+                throw new System.InvalidCastException("O email deve ser valido");
+
+            if (usuarioAcesso.Length < 8)
+                throw new System.InvalidCastException("O usuário de acesso deve ser no minimo 8 caracteres");
+
+            if (senha.Length < 6)
+                throw new System.InvalidCastException("A senha deve ser no minimo 6 caracteres");
+
+            if (!senha.Equals(confirmacaoSenha))
+                throw new System.InvalidCastException("As senhas digitadas não coincidem");
+        }
+
+        #endregion
 
     }
 }
